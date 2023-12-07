d2d7d4771b1b46b532907bd7a754de1c5160b3e3
Hayden Pulchinski <hpulchinski@gmail.com>
2023-12-07 00:27:39 -0500

    Add files via upload

--- stockfish.py
+++ stockfish.py
@@ -1,24 +1,41 @@
 import chess.engine
 
-path = r'C:\PythonChess\stockfish-windows-x86-64-modern\stockfish\stockfish-windows-x86-64-modern'
-
+path = r'C:\Users\pulchihd\Desktop\PythonChess\stockfish-windows-x86-64-modern\stockfish\stockfish-windows-x86-64-modern'
 engine = chess.engine.SimpleEngine.popen_uci(path)
-
 board = chess.Board()
 
-board.push(chess.Move.from_uci('e2e4'))
+game = True
+while game:
+    print(board)
+    try:
+        print("White to play, please enter a move using the UCI protocol.")
+        user_input = input("Move: ")
+        move = chess.Move.from_uci(user_input)
+        if move in board.legal_moves:
+            if len(board.legal_moves) < 1:
+                print("Game over")
+                break
+            board.push(move) 
+        else:
+            print("Illegal move, try again")
+            continue
+    except Exception as e:
+        print("An error occurred:", e)
+        continue
 
-print(board)
+    try:
+        limit = chess.engine.Limit(time=1.0)
+        best_move = engine.play(board, limit=limit)
+        board.push(chess.Move.from_uci(str(best_move.move)))
+    except Exception as e:
+        print("An error occurred:", e)
+        print("ChessBot has resigned")
+        break
+
 
 # Let Stockfish analyze the position
-limit = chess.engine.Limit(time=0.1)  # 0.1 seconds for analysis
-best_move = engine.play(board, limit=limit)
+  # 0.1 seconds for analysis
 
-print("Best move:", best_move.move)
-
-board.push(chess.Move.from_uci('c7c5'))
-
-print(board)
 
 # Close the engine
 engine.quit()
